a314c3f7f0b1a34f0fe5ddf84d8425a1c7b699e0
Szymon Olszak <szymon.olszak3214@gmail.com>
2026-06-04 18:01:41 +0200

    Some random test

---
+++ backend/tests/wind.test.js
@@ -0,0 +1,37 @@
+import request from "supertest";
+import app from "../server.js";
+
+describe("GET /api/wind", () => {
+
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  test("returns wind details", async () => {
+
+    fetch.mockResolvedValue({
+      ok: true,
+      json: async () => ({
+        wind: {
+          direction: {
+            degrees: 270
+          },
+          speed: {
+            value: 36
+          }
+        }
+      })
+    });
+
+    const response = await request(app)
+      .get("/api/wind");
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toEqual({
+      direction: 270,
+      speed: 10
+    });
+  });
+
+});
